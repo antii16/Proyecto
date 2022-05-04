bfe2c7fd08b2998138e5e35fbb933c399b4759a7
antonella <anto@correo.com>
2022-05-04 17:08:11 +0200

    Clase con comentarios y marcas javadoc

--- Ficha_Alumno.java
+++ Ficha_Alumno.java
@@ -4,7 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * @author Antonella
+ * @version 2.0
+ */
 public class Ficha_alumno {
+	
+	/** 
+	 * @param nombre Es el nombre del alumno
+	 * @param apellidos Son los apellidos del alumno
+	 * @param telefono Es el numero de telefono del alumno
+	 * @param email Es el email del alumno
+	 * @param direccion Es la dirección del alumno
+	 * @return Devuelve una lista con los datos de un alumno
+	 */
 	
 public static List<String> añadir_alumno(String nombre, String apellidos, String telefono, String email, String direccion) {
 		
@@ -21,10 +34,13 @@ public static List<String> añadir_alumno(String nombre, String apellidos, String
 		
 	}
 	
+/**
+ * @param args Entrada principal a la aplicación
+ */
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in); 
-		int opcion;
+		int opcion; 
 		List<String> resultado;
 			
 		System.out.println("1. Añadir alumno");
@@ -54,12 +70,12 @@ public static List<String> añadir_alumno(String nombre, String apellidos, String
 			System.out.println("Direccion: ");
 			String direccion  = teclado.next();
 			
-			resultado = añadir_alumno(nombre, apellidos, telefono, email, direccion);
+			resultado = añadir_alumno(nombre, apellidos, telefono, email, direccion); // llamada a la funcion
 			System.out.println("Añadido nuevo alumno:\n" + resultado);
 			
 			
 			
-		case 2:
+		case 2: 
 			System.out.println("SALIR");
 			System.exit(0);
 			teclado.close();
